ce1b757330f79dc9e5524990464a120bf5718898
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-06-03 06:55:34 +0800

    Displayed user's text.

---
+++ app.js
@@ -0,0 +1,11 @@
+const displayText = () => {
+  const inputPage = document.getElementById("input-page");
+  const countPage = document.getElementById("count-page");
+  inputPage.style.display = "none";
+  // get user's inputted text
+  const inputtedText = document.getElementById("inputted-text");
+  const inputtedTextValue = inputtedText.value;
+  // diplay user's inputted text
+  document.getElementById("display-user-text").innerText = inputtedTextValue;
+  countPage.style.display = "block";
+};
