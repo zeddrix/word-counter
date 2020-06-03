857bd5efa32916c3128ee3362c8eab8df32e70ff
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-06-03 11:42:00 +0800

    Added required alert.

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const displayText = () => {
   const inputPage = document.getElementById("input-page");
   const countPage = document.getElementById("count-page");
-  inputPage.style.display = "none";
-  // get user's inputted text
   const inputtedText = document.getElementById("inputted-text");
   const inputtedTextValue = inputtedText.value;
-  // diplay user's inputted text
-  document.getElementById("display-user-text").innerText = inputtedTextValue;
-  countPage.style.display = "block";
+
+  if (inputtedText.value !== "") { // normal flow will continue if the text-area is not empty
+    inputPage.style.display = "none";
+    document.getElementById("display-user-text").innerText = inputtedTextValue;
+    countPage.style.display = "block";
+  } else { // if the text-area is empty, it will issue a warning.
+    alert("This area required")
+  }
+
   console.log(countWords(inputtedTextValue));
 };
 
 const countWords = (str) => {
   return str.split(" ").length;
-}
+};
 
 const renderWordCount = () => {
-   document.getElementById("word-count") = wordCount;
-}
+  document.getElementById("word-count") = wordCount;
+};
