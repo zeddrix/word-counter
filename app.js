ff2b901f30f72b56213c89402718f7d27ac7f72f
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-07-10 07:23:33 +0800

    Event delegatio.

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-const displayText = () => {
-  const inputPage = document.getElementById("input-page");
-  const countPage = document.getElementById("count-page");
-  const text = document.getElementById("text");
-  const textValue = text.value;
+window.addEventListener("click", (event) => {
+  if (event.target.matches("#count-btn")) {
+    displayText();
+  }
+});
 
-  if (text.value !== "") { // normal flow will continue if the text-area is not empty
-    inputPage.style.display = "none";
+const displayText = () => {
+  const wordCounterPage = document.getElementById("word-counter__page");
+  const countPage = document.getElementById("count-page");
+  const textarea = document.getElementById("textarea");
+  const textValue = textarea.value;
+
+  if (textarea.value !== "") {
+    // normal flow will continue if the text-area is not empty
+    wordCounterPage.style.display = "none";
     document.getElementById("display-text").innerText = textValue;
     countPage.style.display = "block";
-  } else { // if the text-area is empty, it will issue a warning.
-    alert("Please enter some text first.")
+  } else {
+    // if the text-area is empty, it will issue a warning.
+    alert("Please enter some text first.");
   }
 
   const countWords = (str) => {
     return str.split(" ").length;
   };
-  const wordCount = (countWords(textValue));
+  const wordCount = countWords(textValue);
 
   const renderWordCount = () => {
     const wordCountDiv = document.getElementById("word-count");
     wordCountDiv.innerHTML = "<h1> Words Counted: " + wordCount + "</h1>";
   };
-  
+
   const getWordDensity = (str) => {
     let wordList = {};
-    str.split(/\s+/).forEach(word => {
-      if(typeof wordList[word] == "undefined"){
+    str.split(/\s+/).forEach((word) => {
+      if (typeof wordList[word] == "undefined") {
         wordList[word] = 1;
-      }
-      else{
+      } else {
         wordList[word]++;
       }
     });
     return wordList;
   };
-  const wordDensity = (getWordDensity(textValue));
-  
+  const wordDensity = getWordDensity(textValue);
+
   const renderWordDensity = () => {
     const wordDensityDiv = document.getElementById("word-density");
-    
+
     let table = "<table>";
-    for(let word in wordDensity){
-      table += "<tr><td>" + word + "</td><td>" + wordDensity[word] + "</td></tr>";
+    for (let word in wordDensity) {
+      table +=
+        "<tr><td>" + word + "</td><td>" + wordDensity[word] + "</td></tr>";
     }
     table += "</table>";
-    
+
     wordDensityDiv.innerHTML = "<h1> Word Density: </h1>" + table;
   };
 
